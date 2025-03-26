565aa16413e7929bef3f64d9d14ea086a790dc3d
lakshya jangid IIITV <202351075@iiitvadodara.ac.in>
2025-03-26 19:23:31 +0530

    MongoDB connection to online database

--- Backend/Config/DB.js
+++ Backend/Config/DB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 const db_connection = async () => {
-    await mongoose.connect(config.MONGO_URL).then(() => {
+    await mongoose.connect(`${config.MONGO_URL}/LMS`).then(() => {
         console.log('Database connected');
     }).catch((err) => {
         console.error(err);
