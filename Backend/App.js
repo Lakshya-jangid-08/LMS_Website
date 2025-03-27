04f34f6ce3996de83769642ca85dd5fb50b49796
lakshya jangid IIITV <202351075@iiitvadodara.ac.in>
2025-03-27 14:24:30 +0530

    App update

--- Backend/App.js
+++ Backend/App.js
@@ -2,11 +2,13 @@ const express = require('express')
 const App = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
+const { clerkWebHooks } = require('./Controllers/webHook')
 dotenv.config()
 App.use(cors())
 
 App.get('/',(req,res)=>{
     res.send('Backend running successfully');
 })
+App.post('/clerk',express.json(),clerkWebHooks)
 
 module.exports = App;
--- Backend/Server.js
+++ Backend/Server.js
@@ -2,7 +2,7 @@ const App = require('./App');
 const DB = require('./Config/DB')
 const config = require('./Config/config')
 const HTTP = require('http')
-const PORT = config.PORT;
+const PORT = process.env.PORT || 5000;
 DB.db_connection();
 
 const Server = HTTP.createServer(App)
